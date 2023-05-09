70b8af4e3f1c9cb0eb5eb44d004e76f9da06896c
Rick De <ar.rickde@gmail.com>
2023-05-09 12:38:22 +0530

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,92 @@
+<script>
+  import Counter from "./Counter.svelte";
+  import welcome from "$lib/images/svelte-welcome.webp";
+  import welcome_fallback from "$lib/images/svelte-welcome.png";
+  import { onMount } from "svelte";
+  import { browser } from "$app/environment";
+  import { createScene } from "../lib/scene.js";
+
+  //   import { bind } from 'svelte/internal';
+
+  let el;
+
+  onMount(() => {
+    createScene(el);
+  });
+
+  // if (browser){
+  // 	createScene(el)
+  // }
+</script>
+
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<html lang="svelte">
+  <section>
+    <span>
+      <canvas id="el1" bind:this={el} />
+    </span>
+    <h1>
+      <span class="welcome">
+        <picture>
+          <source srcset={welcome} type="image/webp" />
+          <img src={welcome_fallback} alt="Welcome" />
+        </picture>
+      </span>
+
+      to your new<br />SvelteKit app
+    </h1>
+
+    <h2>
+      try editing <strong>src/routes/+page.svelte</strong>
+    </h2>
+
+    <Counter />
+  </section>
+</html>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 0.6;
+  }
+
+  h1 {
+    width: 100%;
+  }
+
+  /* .welcome {
+    display: block;
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding: 0 0 calc(100% * 495 / 2048) 0;
+  } */
+
+  /* .welcome img {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    display: block;
+  } */
+  /* canvas {
+    flex: auto;
+    position: absolute;
+    width: 50%;
+    height: 50%;
+    top: 10;
+  }
+  #el1 {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    top: 10;
+  } */
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { createScene2 } from '../../lib/scene2.js';
+	import {onMount} from 'svelte'
+	let el2;
+
+		onMount(()=>{
+		
+		createScene2(el2)
+	})
+</script>
+
+
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About this app</h1>
+<canvas id ="el2" bind:this={el2}  />
+	<p>
+		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
+		following into your command line and following the prompts:
+	</p>
+
+	<pre>npm create svelte@latest</pre>
+
+	<p>
+		The page you're looking at is purely static HTML, with no client-side interactivity needed.
+		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
+		the devtools network panel and reloading.
+	</p>
+
+	<p>
+		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
+		using it with JavaScript disabled!
+	</p>
+</div>
